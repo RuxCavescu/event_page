80dc924b786f3428c6063b1979d80fef0d031975
Adam Řezáč <rezac.adam@email.cz>
2022-05-27 13:59:14 +0200

    featured page done

--- src/js/script.js
+++ src/js/script.js
@@ -17,22 +17,46 @@ export const eventData = async () => {
 
   // console.log(Modals);
 
-
-
-
   // Modals.forEach((e) => {
   //   console.log(e);
   //   document.body.appendChild(e.element)
   // })
   createEvents(usableEventData);
+  featuredEvent(usableEventData);
 };
-
-
 
 eventData();
 
 const createEvents = (usableEventData) => {
   for (let i = 1; i < usableEventData.length; i++) {
-    new Widget(usableEventData[i].name, usableEventData[i].description, usableEventData[i].data, usableEventData[i].image_url, usableEventData[i].id);
+    new Widget(
+      usableEventData[i].name,
+      usableEventData[i].description,
+      usableEventData[i].data,
+      usableEventData[i].image_url,
+      usableEventData[i].id
+    );
   }
+};
+// creating a featured event box
+const featuredEvent = (usableEventData) => {
+  const mainEvent = document.querySelector(".main__event");
+  // const eventContainer = document.createElement("div");
+  // eventContainer.classList.add("event__container");
+  // mainEvent.appendChild(eventContainer);
+  const img = usableEventData[0].image_url;
+  const eventName = usableEventData[0].name;
+  const eventDesc = usableEventData[0].description;
+  console.log(eventName);
+  mainEvent.innerHTML = `
+  <h2>FEATURED EVENT</h2>
+  <img class="event__img" src="${img}" alt="event image">
+  <div class="event__content">
+  <div class="event__text">
+  <h3>${eventName}</h3>
+  <p>"${eventDesc}"</p>
+  </div>
+  <div class="event__button"><button>REGISTER</button></div>
+  </div>
+  `;
 };
