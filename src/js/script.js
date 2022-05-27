774036e53ed99a0b066bac92e960b620b6df2a35
RuxCavescu <rux.cavescu@gmail.com>
2022-05-27 10:17:34 +0200

    pushing api data

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 import { Widget } from './widget';
+
+const eventData = async () => {
+  const response = await fetch(
+    'https://test-api.codingbootcamp.cz/api/597ac8ff/events'
+  );
+  const usableEventData = await response.json();
+
+  console.log(usableEventData);
+};
+
+eventData();
